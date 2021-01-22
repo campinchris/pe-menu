98ccf20f7aef7a0ee74bd5ae5c55a8c12290835e
Xios_Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-22 09:19:02 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 Config = {}
-Config.Key = 166 -- Tecla con la cual de abre el menu, la puedes encontrar aqui: (https://docs.fivem.net/docs/game-references/controls/)
-Config.ComandoRehen = 'th' --Comando para tomar a alguien de rehen
-Config.ComandoCargar1 = 'carry'--Comando para cargar a alguien 2 
-Config.ComandoCargar2 = 'carry2'--Comando para cargar a alguien 2
+Config.Locale               = 'es'
+Config.Key                  = 166 -- Tecla con la cual de abre el menu, la puedes encontrar aqui: (https://docs.fivem.net/docs/game-references/controls/)
+Config.ComandoRehen         = 'th' --Comando para tomar a alguien de rehen
+Config.ComandoCargar1       = 'carry'--Comando para cargar a alguien 2 
+Config.ComandoCargar2       = 'carry2'--Comando para cargar a alguien 
